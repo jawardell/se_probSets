40e789e62a3438125797aff18783e485628b1377
Joanne Wardell <jawardell@valdosta.edu>
2018-10-24 04:08:55 -0400

    adds work

---
+++ prob1/src/prob1/AddEmployeeTest.java
@@ -0,0 +1,54 @@
+package prob1;
+
+import org.junit.*;
+
+public class AddEmployeeTest {
+	Employee emp = new Employee("Bob", 17);
+	Store store = new Store();
+	
+	@Test
+	public void test1() {
+		
+	}
+	
+	@Test
+	public void test2() {
+		
+	}
+	
+	@Test
+	public void test3() {
+		
+	}
+	
+	@Test
+	public void test4() {
+		
+	}
+	
+	@Test
+	public void test5() {
+		
+	}
+	
+	@Test
+	public void test6() {
+		
+	}
+	
+	@Test
+	public void test7() {
+		
+	}
+	
+	@Test
+	public void test8() {
+		
+	}
+	
+	@Test
+	public void test9() {
+		
+	}
+	
+}
